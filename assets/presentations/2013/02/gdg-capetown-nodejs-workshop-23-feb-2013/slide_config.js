4c044ee2cd575edacd9cb5934e5e05ce40cc114e
Johann du Toit <exxonno@gmail.com>
2013-02-20 11:04:33 +0200

    Got most of the slideshow up.
    Did some improvements on how I show concurrency.

--- assets/presentations/2013/02/gdg-capetown-nodejs-workshop-23-feb-2013/slide_config.js
+++ assets/presentations/2013/02/gdg-capetown-nodejs-workshop-23-feb-2013/slide_config.js
@@ -8,12 +8,11 @@ var SLIDE_CONFIG = {
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/js_logo.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
-    ],
-    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+    ]
   },
 
   // Author information
@@ -24,13 +23,6 @@ var SLIDE_CONFIG = {
     twitter: '',
     www: 'http://www.johanndutoit.net',
     github: 'http://github.com/johanndutoit'
-  }/*, {
-    name: 'Second Name',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  }*/]
+  }]
 };
 
