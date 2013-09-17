a4f3551d2cf5d49d7e9e93fe1d2fc8f25bccc211
Johann du Toit <exxonno@gmail.com>
2013-09-17 22:09:00 +0200

    Added the humanipo presentation

---
+++ assets/presentations/2013/09/humanipo-google-apps-for-startups-21-09-2013/slide_config.js
@@ -0,0 +1,35 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Google Apps for Business / Startups',
+    subtitle: 'http://goo.gl/LZmSt',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_logo_3D_online_small.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Hiren Patel',
+    company: 'Freelancer',
+    gplus: 'https://plus.google.com/117104219955586986512',
+    twitter: '',
+    www: 'http://www.johanndutoit.net',
+    github: 'http://github.com/johanndutoit'
+  },{
+    name: 'Johann du Toit',
+    company: 'Digital Pro',
+    gplus: 'https://plus.google.com/117104219955586986512',
+    twitter: '',
+    www: 'http://www.johanndutoit.net',
+    github: 'http://github.com/johanndutoit'
+  }]
+};
+
