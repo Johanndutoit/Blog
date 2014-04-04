88d6545fd0df17cb0fb0f1e8b83b55683d12e3f7
Johann du Toit <exxonno@gmail.com>
2014-04-04 21:00:37 +0200

    Fixed a few last minute issues

--- assets/presentations/2014/04/realtime-apps-05-april-2014/slide_config.js
+++ assets/presentations/2014/04/realtime-apps-05-april-2014/slide_config.js
@@ -2,7 +2,7 @@ var SLIDE_CONFIG = {
   // Slide settings
   settings: {
     title: 'Realtime apps - lessons while working towards deploy',
-    subtitle: 'http://goo.gl/oKBA49',
+    subtitle: 'http://goo.gl/bWwd3d',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
